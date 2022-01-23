0529b279824c05d24ac611ef94edb58abde650ae
Schneider <schneidershades@gmail.com>
2022-01-23 13:36:14 +0100

    push

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -61,7 +61,7 @@
                 <img src='{{ URL::to("assets/img/img-mobile-me.jpg")}}' class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="my picture">
                 <h1 class="text-uppercase poppins-font">I'm Schneider Komolafe.<span> Software Engineering Manager</span></h1>
                 <p class="open-sans-font">I'm a software engineer manager focused on crafting clean & user‑friendly experiences within teams, 
-                    I am passionate about building excellent software that improves the lives of those it evolves around. Contact me i am open for opportunities</p>
+                    I am passionate about building excellent software that improves the lives of those it evolves around. Contact me i am open to new opportunities</p>
                 <a class="button" href="/portfolio">
                     <span class="button-text"> my portfolio</span>
                     <span class="button-icon fa fa-arrow-right"></span>
